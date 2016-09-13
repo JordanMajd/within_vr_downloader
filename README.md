2f6aa678787c5e821d4d82d911caddf69edf4365
Jordan Majd <jordan.majd@galvanize.com>
2016-09-13 16:39:41 -0600

    updates site to angular

--- README.md
+++ README.md
@@ -1,30 +1,55 @@
-# Within Player
+# Within VR Downloader
 
-To rip videos from Within Web Player format URLs as such:
+:danger: **This project is not affiliated or endorsed by Within in any way, nor does it host any of their content.** :danger:
 
-```
-http://player.with.in/6-EVOL/video/<filename>-WEBPLAYER_PROGRESSIVE-<stereo/mono>-<resolution>.<filetype>
-```
+Within VR Downloader is a tool to download HD Stereoscopic 360 VR videos from Within VR.
 
-Supported resolutions:
+## Spec
 
-- 640x320
-- 1280x640
-- 1920x960
-- 2880x1440
-- 4096x2048L
-- 4096x2048H
+The tool supports downloading the following formats.
 
-Supported video types:
+### Supported Video Resolutions
+
+- 320P: 640x320
+- 640P: 1280x640
+- 960P: 1920x960
+- 2K:   2880x1440
+- 4K:   4096x2048L
+- 4K:   4096x2048H
+
+### Supported Projection Types
+
+As of writing, most stereo files are over under.
 
 - stereo
 - mono
 
-
-Supported file types:
+### Supported Video File Types
 
 - mp4
 - webm
 
+### Supported Audio Types
 
-Get filenames from `unified.json`, `content[n].cameras.default.webplayer.name`
+- mp3
+
+## Getting started
+
+1. Clone the project.
+1. Install dependencies with `npm install`.
+1. Run the development server with `npm start`.
+1. Open browser to localhost:8080
+
+## Using `wget` or `curl`
+
+`wget` or `curl` can be used instead of the web interface to download the movie files.
+
+To rip videos from Within Web Player format URLs as such:
+
+```bash
+wget http://player.with.in/6-EVOL/video/<filename>-WEBPLAYER_PROGRESSIVE-<stereo/mono>-<resolution>.<filetype>
+```
+
+Filenames can be pulled from from `unified.json`, `content[n].cameras.default.webplayer.name`
+
+## License
---
+++ res/app.js
@@ -0,0 +1,172 @@
+(function() {
+  'use strict';
+
+  angular.module('WithinDownloader', []);
+
+}());
+
+(function() {
+  'use strict';
+
+  angular.module('WithinDownloader')
+    .controller('WithinController', ['$window', 'WithinService', MainController]);
+
+  function MainController($window, WithinService) {
+
+    var vm = this;
+
+    vm.videoData = [];
+
+    vm.settings = {
+      resolutions: ['640x320', '1280x640', '1920x960', '2880x1440', '4096x2048L', '4096x2048H'],
+      projections: ['stereo', 'mono'],
+      fileTypes: ['mp4', 'webm']
+    };
+
+    vm.selectedSettings = {
+      resolution: "",
+      projection: "",
+      fileType: "",
+      downloadAudio: "true"
+    };
+
+    vm.getImagePath = getImagePath;
+    vm.getVideoPath = getVideoPath;
+    vm.getAudioPaths = getAudioPaths;
+    vm.downloadFiles = downloadFiles;
+
+    init();
+
+    function init() {
+      WithinService.getWithinMetadata().then(function(_videoData) {
+        vm.videoData = _videoData;
+      });
+    }
+
+    function getVideoPath(video) {
+
+      return "http://player.with.in/" + video.root + "video/" + video.cameras.default.webplayer.name + "-WEBPLAYER_PROGRESSIVE-" + vm.selectedSettings.projection + "-" + vm.selectedSettings.resolution + "." + vm.selectedSettings.fileType;
+    }
+
+    function getImagePath(video) {
+      return "http://with.in/images/projects/" + video.root.slice(0, -1) + ".jpg";
+    }
+
+    function getAudioPaths(video) {
+
+    }
+
+    function downloadFiles(video) {
+
+      if (vm.selectedSettings.fileType !== "" && vm.selectedSettings.resolution !== "" && vm.selectedSettings.projection !== "") {
+
+        vm.error = "";
+        var videoPath = getVideoPath(video);
+        $window.open(videoPath, "Download");
+
+        if (vm.selectedSettings.downloadAudio === "true") {
+
+        }
+      } else {
+        vm.error = "Please select file type, resolution and projection";
+      }
+
+    }
+
+  }
+}());
+
+(function() {
+  'use strict';
+
+  angular.module('WithinDownloader')
+    .service('WithinService', ['$http', WithinService]);
+
+  function WithinService($http) {
+
+    var self = this;
+
+    self.getWithinMetadata = getWithinMetadata;
+
+    return self;
+
+    function getWithinMetadata() {
+
+      return $http.get("http://with.in/unified.json").then(function(data) {
+
+        var content = data.data.content;
+        var videoData = [];
+
+        for (var key in content) {
+          if (content.hasOwnProperty(key)) {
+
+            var video = content[key];
+
+            if (video.visible_on.webplayer === true) {
+              videoData.push(video);
+            }
+          }
+        }
+
+        return videoData;
+      });
+    }
+  }
+}());
+
+
+// (function() {
+//   'use strict';
+//
+//   function WithinDownloader() {
+//
+//   }
+//
+//   WithinDownloader.prototype.getWithinMetadata = function(cb) {
+//     this.getHTTP("http://with.in/unified.json", cb);
+//   };
+//
+// WithinDownloader.prototype.getVideoPath = function(name, display, resolution, format) {
+//     return "http://player.with.in/6-EVOL/video/" + name + "-WEBPLAYER_PROGRESSIVE-" + display + "-" + resolution + "." + format;
+//   };
+//
+//   WithinDownloader.prototype.getAudioPaths = function(name) {
+//
+//   };
+//
+//   WithinDownloader.prototype.getImagePath = function(name){
+//
+//   };
+//
+//   WithinDownloader.prototype.getHTTP = function(path, onSuccess, onFailure) {
+//
+//     var request = new XMLHttpRequest();
+//     request.open("GET", path, true);
+//
+//     request.onreadystatechange = function() {
+//       if (this.readyState === 4) {
+//         if (this.status >= 200 && this.status < 400) {
+//           var data = JSON.parse(this.responseText);
+//           onSuccess(data);
+//         } else {
+//           onFailure(this);
+//         }
+//       }
+//     };
+//     request.send();
+//   };
+//
+//   var wd = new WithinDownloader();
+//   wd.getWithinMetadata(onMetaDataLoaded);
+//
+//   function onMetaDataLoaded(data){
+//     var metaData = data.content;
+//     for(var key in metaData){
+//       if(metaData.hasOwnProperty(key)){
+//         addVideoPanel(metaData[key]);
+//       }
+//     }
+//   }
+//
+//
+// }());
